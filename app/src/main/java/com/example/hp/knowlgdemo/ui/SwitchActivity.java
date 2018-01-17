eba0be7a262ed683764525fdfcb8d4f0d9e78229
2236066560 <2236066560@qq.com>
2018-01-17 17:21:10 +0800

    日期时间设置

---
+++ app/src/main/java/com/example/hp/knowlgdemo/ui/SwitchActivity.java
@@ -0,0 +1,90 @@
+package com.example.hp.knowlgdemo.ui;
+
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import com.example.hp.knowlgdemo.R;
+
+/**
+ * Created by Administrator on 2018/1/17.
+ */
+
+public class SwitchActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener, View.OnClickListener, Chronometer.OnChronometerTickListener
+{
+    private ToggleButton tbtn_open;
+    private Switch swh_status;
+    private Chronometer chronometer;
+    private Button btn_start,btn_stop,btn_base,btn_format;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_switch);
+        tbtn_open = (ToggleButton) findViewById(R.id.tbtn_open);
+        swh_status = (Switch) findViewById(R.id.swh_status);
+        tbtn_open.setOnCheckedChangeListener(this);
+        swh_status.setOnCheckedChangeListener(this);
+        chronometer = (Chronometer) findViewById(R.id.chronometer);
+        btn_start = (Button) findViewById(R.id.btnStart);
+        btn_stop = (Button) findViewById(R.id.btnStop);
+        btn_base = (Button) findViewById(R.id.btnReset);
+        btn_format = (Button) findViewById(R.id.btn_format);
+
+        chronometer.setOnChronometerTickListener(this);
+        btn_start.setOnClickListener(this);
+        btn_stop.setOnClickListener(this);
+        btn_base.setOnClickListener(this);
+        btn_format.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        switch (buttonView.getId()){
+            case R.id.tbtn_open:
+                if(buttonView.isChecked()) Toast.makeText(this,"打开声音",Toast.LENGTH_SHORT).show();
+                else Toast.makeText(this,"打开声音",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.swh_status:
+                if(buttonView.isChecked()) Toast.makeText(this,"开关:ON",Toast.LENGTH_SHORT).show();
+                else Toast.makeText(this,"开关:OFF",Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnStart:
+                chronometer.start();// 开始计时
+                break;
+            case R.id.btnStop:
+                chronometer.stop();// 停止计时
+                break;
+            case R.id.btnReset:
+                chronometer.setBase(SystemClock.elapsedRealtime());// 复位
+                break;
+            case R.id.btn_format:
+                chronometer.setFormat("Time：%s");// 更改时间显示格式
+                break;
+        }
+    }
+
+    @Override
+    public void onChronometerTick(Chronometer chronometer) {
+        String time = chronometer.getText().toString();
+        if(time.equals("00:00")){
+            Toast.makeText(SwitchActivity.this,"时间到了~",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
